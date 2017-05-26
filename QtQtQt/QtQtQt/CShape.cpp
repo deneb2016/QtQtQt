d20d996fae668d67d4a19958dc85e8a9853b3bba
Owner-PC\Owner <dayg502@gmail.com>
2017-05-26 19:09:23 +0900

    CShape 헤더

--- QtQtQt/QtQtQt/CShape.cpp
+++ QtQtQt/QtQtQt/CShape.cpp
@@ -1,2 +1,53 @@
-#include "stdafx.h"
 #include "CShape.h"
+#include <math.h>
+/*CShape*/
+CShape::CShape(CPoint lu, CPoint rd, COLORREF bc, COLORREF pc) : m_leftup(lu), m_rightdown(rd), m_brushColor(bc), m_penColor(pc) {};
+
+void CShape::setLeftUp(CPoint pos){ 
+	if (pos.x >= m_rightdown.x || pos.y <= m_rightdown.y) return; //유효하지 않은 경우 그냥 리턴
+
+	m_leftup = pos;
+}
+
+void CShape::setRightDown(CPoint pos) {
+	if (pos.x <= m_leftup.x || pos.y >= m_leftup.y) return;//유효하지 않은 경우 그냥 리턴
+	m_rightdown = pos;
+}
+void CShape::setBrushColor(COLORREF color) {
+	m_brushColor = color;
+}
+void CShape::setPenColor(COLORREF color) {
+	m_penColor = color;
+}
+
+bool CShape::edgeCheck(CPoint pos) {
+	if ((pos.x >= m_leftup.x && pos.x <= m_rightdown.x) //x 영역 안
+		&& (pos.y == m_leftup.y || pos.y == m_leftup.y)) //y좌표 위
+		return 1;
+	if ((pos.y <= m_leftup.y && pos.y >= m_rightdown.y) //y 영역 안
+		&& (pos.x == m_leftup.x || pos.x == m_leftup.x)) //x좌표 위
+		return 1;
+
+	return 0;
+}
+
+
+
+CCircle::CCircle(CPoint lu, CPoint rd, COLORREF bc, COLORREF pc) :CShape(lu, rd, bc, pc) {};
+CRect::CRect(CPoint lu, CPoint rd, COLORREF bc, COLORREF pc) :CShape(lu, rd, bc, pc) {};
+CTriangle::CTriangle(CPoint lu, CPoint rd, COLORREF bc, COLORREF pc) :CShape(lu, rd, bc, pc) {};
+
+bool CCircle::select(CPoint pos) {
+	//c^2 = a^2 - b^2;
+	CPoint center((m_leftup.x + m_rightdown.x) / 2, (m_leftup.y + m_rightdown.y) / 2);
+	int cSquare = ((m_leftup.y - m_rightdown.y) / 2) * ((m_leftup.y - m_rightdown.y) / 2) - ((m_rightdown.x - m_leftup.x) / 2) *((m_rightdown.x - m_leftup.x ) /2 );
+	double c = sqrt(cSquare);
+		
+
+	
+};
+bool CRect::select(CPoint pos) {
+	if (pos.x >= m_leftup.x && pos.x <= m_rightdown.x && pos.y >= m_rightdown.y && pos.y <= m_leftup.y)
+		return 1;
+	else return 0;
+};
--- QtQtQt/QtQtQt/CShape.h
+++ QtQtQt/QtQtQt/CShape.h
@@ -1,1 +1,36 @@
 #pragma once
+
+class CShape {
+private:
+	CPoint m_leftup, m_rightdown; 
+	COLORREF m_brushColor, m_penColor; // 테두리 색, 내부 색, 투명도
+
+public:
+	CShape(CPoint lu, CPoint rd, COLORREF bc, COLORREF pc);
+	virtual bool select(CPoint position) ; // 좌표가 도형 안에 있으면 true, 아니면 false
+	virtual void move(CPoint vec) ; // vec만큼 - 이동
+	void setLeftUp(CPoint pos) ; // m_leftup 변수를 바꾸는데, m_rightdown보다 오른쪽이거나 밑인지 검사
+	void setRightDown(CPoint pos) ; // m_rightdown 변수를 바꾸는데, m_leftup보다 왼쪽이거나 위인지 검사
+	void setBrushColor(COLORREF color) ; // 내부색 바꾸기
+	void setPenColor(COLORREF color) ; // 테두리색 바꾸기
+	virtual void redraw(CClientDC & dc, bool selected) ; // 자기자신 그리기, selected이면 강조
+	bool edgeCheck(CPoint pos) ; // 가장자리 부근에 있는지 체크
+};
+
+class CCircle : public CShape {
+private:
+public:
+	CCircle(CPoint lu, CPoint rd, COLORREF bc, COLORREF pc);
+};
+
+class CRectangle : public CShape {
+private:
+public:
+	CRectangle(CPoint lu, CPoint rd, COLORREF bc, COLORREF pc);
+};
+
+class CTriangle : public CShape {
+private:
+public:
+	CTriangle(CPoint lu, CPoint rd, COLORREF bc, COLORREF pc);
+};
