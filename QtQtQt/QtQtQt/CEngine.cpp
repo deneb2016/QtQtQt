00f99d0db3644dc4ef192ca7700d3870c2c831a9
이승관 <deneb2016@gmail.com>
2017-05-26 19:50:29 +0900

    no message

--- QtQtQt/QtQtQt/CEngine.cpp
+++ QtQtQt/QtQtQt/CEngine.cpp
@@ -1,2 +1,19 @@
 #include "stdafx.h"
 #include "CEngine.h"
+
+void CEngine::createCircle(CPoint lu, CPoint ld, COLORREF brush, COLORREF pen)
+{
+	objects.push_back(CCircle(lu, ld, brush, pen));
+}
+
+void CEngine::createRectangle(CPoint lu, CPoint ld, COLORREF brush, COLORREF pen)
+{
+	objects.push_back(CRectangle(lu, ld, brush, pen));
+}
+
+void CEngine::createTriangle(CPoint u, CPoint v, CPoint w, COLORREF brush, COLORREF pen)
+{
+	objects.push_back(CTriangle(u, v, w, brush, pen));
+}
+
+
