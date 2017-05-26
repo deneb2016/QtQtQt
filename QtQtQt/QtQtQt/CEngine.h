af1347132f87912f3308aa97ea176a54b962b5d4
이승관 <deneb2016@gmail.com>
2017-05-26 19:25:42 +0900

    CEngine.h

--- QtQtQt/QtQtQt/CEngine.h
+++ QtQtQt/QtQtQt/CEngine.h
@@ -1,1 +1,45 @@
 #pragma once
+#include <list>
+#include <vector>
+#include "CShape.h"
+
+using namespace std;
+class CKey {
+private:
+	std::list<CShape>::iterator iter;
+	bool isValid;
+
+public:
+	CKey() {
+		isValid = false;
+	}
+	bool isNull() {
+		return !isValid;
+	}
+	void setIter(std::list<CShape>::iterator it) {
+		iter = it;
+		isValid = true;
+	}
+	void setNull() {
+		isValid = false;
+	}
+	std::list<CShape>::iterator getIter() {
+		return iter;
+	}
+};
+class CEngine {
+private:
+	std::list<CShape> objects;
+
+public:
+	void createCircle(CPoint lu, CPoint ld, COLORREF brush, COLORREF pen);
+	void createRect(CPoint lu, CPoint rd, COLORREF brush, COLORREF pen);
+	void createTriangle(CPoint u, CPoint v, CPoint w, COLORREF brush, COLORREF pen);
+
+	void deleteObjects(vector<CKey>& dels);
+	void redrawAllObjects(CClientDC& dc, vector<list<CShape>::iterator>& selectedObjects); //: 모든 오브젝트에 대해 redraw호출
+	CKey find(CPoint pos) //pos에 있는 오브젝트를 찾아서 키값 리턴
+	void moveObjects(vector<CKey>& objects, CPoint moveVector)// : objects들 이동
+
+
+};
